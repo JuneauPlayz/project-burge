d86916471061640292bdf81f096d735389aeab29
JuneauPlayz <cjquan02@gmail.com>
2024-12-27 01:28:56 -0800

    shocked

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -5,7 +5,11 @@ var curr_elem = ""
 @onready var label: Label = $Label
 @onready var timer: Timer = $Label/Timer
 @onready var currelem: Label = $currelem
-
+var delayed_multiplier = 1
+var delayed_countdown = 0
+var delay = false
+@onready var damage_num: Label = $DamageNum
+@onready var d_timer: Timer = $DamageNum/dTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,18 +18,37 @@ func _ready() -> void:
 
 func calc_damage(damage, element):
 	var multiplier = 1
+	var totaldmg = 0
 	match reaction(element):
 		"vaporize":
 			popup("Vaporize!")
 			currelem.text = "current element: nothing"
 			multiplier = 2
+		"shock":
+			popup("shocked!")
+			currelem.text = "current element: nothing"
+			delayed_multiplier = 1.5
+			delayed_countdown = 3
+			delay = true
 		_:
 			curr_elem = element
 			currelem.text = "current element: " + element
 			
-	hp -= (damage * multiplier)
+	
+	if (delay):
+		if (delayed_countdown > 0):
+			delayed_countdown -= 1
+			if (delayed_countdown == 0):
+				totaldmg = (damage * multiplier * delayed_multiplier)
+				delay = false
+			else:
+				totaldmg = (damage * multiplier)
+	else:
+		totaldmg = (damage * multiplier)
+	
+	hp -= totaldmg
 	progress_bar.value = hp
-
+	dpopup(str(totaldmg) + "!")
 			
 		
 		
@@ -35,6 +58,10 @@ func reaction(elem):
 		match elem:
 			"water":
 				return "vaporize"
+	if (curr_elem == "water"):
+		match elem:
+			"lightning":
+				return "shock"
 	
 
 func _on_fire_pressed() -> void:
@@ -55,7 +82,14 @@ func popup(text):
 	label.text = text
 	timer.start()
 	
-
+func dpopup(text):
+	damage_num.visible = true
+	damage_num.text = text
+	d_timer.start()
 
 func _on_timer_timeout() -> void:
 	label.visible = false
+
+
+func _on_d_timer_timeout() -> void:
+	damage_num.visible = false
