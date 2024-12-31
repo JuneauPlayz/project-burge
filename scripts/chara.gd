f7d5fe3da3d9d1d539ad1ecaecdeb80c0d0f2857
Benjamin K. <benjaminkurokawa@gmail.com>
2024-12-31 02:07:21 -0800

    Update chara.gd

--- scripts/chara.gd
+++ scripts/chara.gd
@@ -1,1 +1,17 @@
+class_name Chara
+
 extends Node
+
+var hp = 100
+var skills = []
+var basic = skills[0]
+var skill1 = skills[1]
+var skill2 = skills[2]
+var ult = skills[3]
+
+func use_skill(target):
+	pass
+	
+func use_skill_special(target, option):
+	pass
+	
