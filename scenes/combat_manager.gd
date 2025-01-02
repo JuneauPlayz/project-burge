88f659714210c12acc809fc7091a6d08009227b3
Benjamin K. <benjaminkurokawa@gmail.com>
2025-01-02 02:28:01 -0800

    idk

--- scenes/combat_manager.gd
+++ scenes/combat_manager.gd
@@ -40,3 +40,33 @@ func _on_basic_pressed() -> void:
 	
 	#testing output
 	print("basic attack landed! remaining hp: " + str(enemy1.health))
+	
+func execute_turn():
+	pass
+	
+func ally1_basic():
+	var skill = ally1.basic_atk
+	var damage : float
+	enemy1.take_element(skill.element)
+	enemy1.take_damage(skill.damage)
+	
+	#testing output
+	print("basic attack landed! remaining hp: " + str(enemy1.health))
+
+func ally1_skill1():
+	var skill = ally1.skill_1
+	enemy1.take_element(skill.element)
+	enemy1.take_damage(skill.damage)
+	print("skill 1 landed! remaining hp: " + str(enemy1.health))
+	
+func ally1_skill2():
+	var skill = ally1.skill_2
+	enemy1.take_element(skill.element)
+	enemy1.take_damage(skill.damage)
+	print("skill 2 landed! remaining hp: " + str(enemy1.health))
+	
+func ally1_ult():
+	var skill = ally1.ult
+	enemy1.take_element(skill.element)
+	enemy1.take_damage(skill.damage)
+	print("ultimate landed! remaining hp: " + str(enemy1.health))
